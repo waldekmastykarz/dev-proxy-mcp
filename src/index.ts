34d35fdb00f256114f6824bbf6ff64fac1ea1a9e
Waldek Mastykarz <waldek@mastykarz.nl>
2025-05-05 08:57:58 +0200

    Adds tool annotations and updates descriptions

--- src/index.ts
+++ src/index.ts
@@ -17,15 +17,21 @@ const server = new McpServer({
 
 server.tool('FindDocs', 'Finds the relevant Dev Proxy documentation for the given query',
   {
-    query: z.string().describe('The search query to find documentation for'),
+    query: z.string().describe('The keyword search query to find documentation for'),
     version: z.string().optional().describe('Dev Proxy version to get documentation for in the format x.y.z, eg. 0.27.0')
+  },
+  {
+    title: 'Find docs'
   },
   async ({ query, version }) => ({
     content: [{ type: 'text', text: await findDocs(query, version === null ? undefined : version) }]
   })
 );
 
-server.tool('GetVersion', 'Gets the current Dev Proxy version',
+server.tool('GetVersion', 'Gets the currently installed Dev Proxy version',
+  {
+    title: 'Get version',
+  },
   async () => ({
     content: [{ type: 'text', text: await getVersion() }]
   })
